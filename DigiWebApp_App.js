53ccea1822063844fc8e1f6d7dfc07b17bffda36
André Frick <drscheme123@users.sf.net>
2012-10-21 20:18:10 +0200

    new build debug

--- DigiWebApp_App.js
+++ DigiWebApp_App.js
@@ -5278,7 +5278,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 2445
+    , softwareVersion: 2446
 
 
     /**
@@ -10117,7 +10117,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         }),
 
         buildLabel: M.LabelView.design({
-            value: 'Build: 2445',
+            value: 'Build: 2446',
             cssClass: 'infoLabel marginBottom25 unselectable'
         }),
 
