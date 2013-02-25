7a615a452bd85dfaa782b2fdc91191c9c15f4b3f
DIGI Zeiterfassung GmbH <support@digi-zeiterfassung.de>
2013-02-25 10:13:16 +0100

    new build debug

--- DigiWebApp_App.js
+++ DigiWebApp_App.js
@@ -4938,7 +4938,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 2962
+    , softwareVersion: 2964
 
 
     /**
@@ -11939,8 +11939,15 @@ DigiWebApp.BautagebuchEinstellungenController = M.Controller.extend({
 			rec.save();
 		} else {
 			var rec = DigiWebApp.BautagebuchEinstellungen.find()[0];
-			that.settings.startUhrzeit = rec.get("startUhrzeit");
-			that.settings.inStundenBuchen = rec.get("inStundenBuchen");
+			if (typeof(rec.get("startUhrzeit")) !== "undefined") that.settings.startUhrzeit = rec.get("startUhrzeit");
+			if (typeof(rec.get("inStundenBuchen")) !== "undefined") {
+				that.settings.inStundenBuchen = rec.get("inStundenBuchen");
+				that.settings.inStundenBuchenItem = [{
+			        value: 'inStundenBuchen'
+			      , label: M.I18N.l('BautagebuchInStundenBuchen')
+			      , isSelected: rec.get("inStundenBuchen")
+				}];
+			}
 		}
 	}
 	
@@ -14432,7 +14439,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 2962'
+              value: 'Build: 2964'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
