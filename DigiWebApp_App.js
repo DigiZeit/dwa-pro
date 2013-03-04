f6b907d7c59a0fc1ebdafa8fcee2de83e06c3337
DIGI Zeiterfassung GmbH <support@digi-zeiterfassung.de>
2013-03-04 11:14:30 +0100

    new build debug

--- DigiWebApp_App.js
+++ DigiWebApp_App.js
@@ -4651,7 +4651,7 @@ DigiWebApp.BautagebuchZeitenDetailsController = M.Controller.extend({
 		    	}
 		    });
 		    taetigkeitenArray = _.compact(taetigkeitenArray);
-		    taetigkeitenArray.push({label: M.I18N.l('selectSomethingOptional'), value: '0', isSelected: !itemSelected});
+		    taetigkeitenArray.push({label: M.I18N.l('selectSomething'), value: '0', isSelected: !itemSelected});
 			that.set("activityList", taetigkeitenArray)
 		}
 	}
@@ -5192,7 +5192,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 3090
+    , softwareVersion: 3091
 
 
     /**
@@ -15020,7 +15020,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 3090'
+              value: 'Build: 3091'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
