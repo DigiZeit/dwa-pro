7c2d9459de01f2ea8cb9f57a0d3111a9756a75f5
DIGI Zeiterfassung GmbH <support@digi-zeiterfassung.de>
2013-03-19 09:04:41 +0100

    new build debug

--- DigiWebApp_App.js
+++ DigiWebApp_App.js
@@ -4943,7 +4943,7 @@ DigiWebApp.DashboardController = M.Controller.extend({
     			// evtl. Fehlermeldung, dass noch eine Datenübertragung läuft bzw. nur alle 30 Sekunden eine Datenübertragung gestartet werden darf
     		}
     	}
-    	if (startTransfer === YES) {
+    	if (startTransfer === YES || DigiWebApp.DashboardController.lastTimestampDatatransfer === null) {
     		DigiWebApp.DashboardController.set("lastTimestampDatatransfer") = D8.now().getTimestamp();
 	        var bookings = DigiWebApp.Booking.find();
 	        if(bookings.length > 0) {
@@ -6235,7 +6235,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 3263
+    , softwareVersion: 3264
 
 
     /**
@@ -16732,7 +16732,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 3263'
+              value: 'Build: 3264'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
