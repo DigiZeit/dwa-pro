c12eee46e99d65f67f1f5dde59cd6e0dae5a17d2
DIGI Zeiterfassung GmbH <support@digi-zeiterfassung.de>
2013-08-15 13:33:13 +0200

    new build debug

--- DigiWebApp_App.js
+++ DigiWebApp_App.js
@@ -7222,7 +7222,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 3875
+    , softwareVersion: 3876
 
 
     /**
@@ -20170,7 +20170,7 @@ DigiWebApp.InfoPage = M.PageView.design({
           childViews: 'logo languageSelection spacer8 companyIdLabel mitarbeiterNameVornameLabel spacer9 versionLabel cordovaVersionLabel companyLabel streetLabel cityLabel emailLabel1 spacer1 emailLabel2 spacer2 spacer3 TMPVersionLabel spacer4 deviceinfo spacer5 connectioninfo spacer6 environmentinfo spacer7 localStorageUsage'
 
         , logo: M.ImageView.design({
-              value: 'theme/images/NeuesLogo2013.png'
+              value: 'theme/images/NeuesLogo2013_white.png'
             , cssClass: 'logoInfoPage marginBottom25 unselectable'
         })
 
@@ -20281,7 +20281,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 3875'
+              value: 'Build: 3876'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
