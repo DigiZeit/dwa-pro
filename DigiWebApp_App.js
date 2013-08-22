abcb8bbdf20217e4156e31e85d455d190a4e154e
DIGI Zeiterfassung GmbH <support@digi-zeiterfassung.de>
2013-08-22 15:06:17 +0200

    new build debug

--- DigiWebApp_App.js
+++ DigiWebApp_App.js
@@ -7396,7 +7396,7 @@ DigiWebApp.RequestController = M.Controller.extend({
      */
     , errorCallback: {}
     
-    , softwareVersion: 3977
+    , softwareVersion: 3978
 
 
     /**
@@ -20592,7 +20592,7 @@ DigiWebApp.InfoPage = M.PageView.design({
         })
 
         , buildLabel: M.LabelView.design({
-              value: 'Build: 3977'
+              value: 'Build: 3978'
             , cssClass: 'infoLabel marginBottom25 unselectable'
         })
 
